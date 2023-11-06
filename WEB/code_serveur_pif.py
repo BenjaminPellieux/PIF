2c7a8163d82a4841e035fd4851c0879bbf7ff645
BenjaminPellieux <hyophyse8@gmail.com>
2023-11-06 12:14:57 +0100

    Add image for controle

--- WEB/code_serveur_pif.py
+++ WEB/code_serveur_pif.py
@@ -3,8 +3,25 @@ from flask import Flask, render_template
 app = Flask(__name__, static_folder='static')
 
 @app.route('/')
-def test():
-    return render_template("test.html")
+def index():
+    return render_template("index.html")
+
+@app.route('/vision')
+def vision():
+    return render_template("vision.html")
+
+@app.route('/gps')
+def gps():
+    return render_template("gps.html")
+
+@app.route('/controle')
+def controle():
+    return render_template("controle.html")
+
+@app.route('/aspiration')
+def aspiration():
+    return render_template("aspiration.html")
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080)
