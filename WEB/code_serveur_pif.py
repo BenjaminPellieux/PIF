92dcae55ecf0368b1a29cb802e82cfadbfb2fa12
BenjaminPellieux <hyophyse8@gmail.com>
2023-11-06 13:51:28 +0100

    add command function from web to serv

--- WEB/code_serveur_pif.py
+++ WEB/code_serveur_pif.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, after_this_request, request
 
 app = Flask(__name__, static_folder='static')
+
+
+
 
 @app.route('/')
 def index():
@@ -22,6 +25,14 @@ def controle():
 def aspiration():
     return render_template("aspiration.html")
 
+
+@app.route('/command', methods=['POST'])
+def command():
+    #response.headers['Access-Control-Allow-Origin'] = '*'
+    #input_json = request.get_json(force=True) 
+    com = request.form["javascript_data"] 
+    print(f"[DEBUG] Reciv command : {com=}") 
+    return "200"
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080)
