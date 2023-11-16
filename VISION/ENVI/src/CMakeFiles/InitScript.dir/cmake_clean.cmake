0201eac33043b0ab3a04503ffd169eb2a71dd21b
Bakagamin <froment.guillaume.91@gmail.com>
2023-11-16 11:14:11 +0100

    setup branche guillaume

---
+++ VISION/ENVI/src/CMakeFiles/InitScript.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/InitScript.dir/InitScript.cpp.o"
+  "InitScript"
+  "InitScript.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/InitScript.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
