7ddaec0aa592118610b76c2768ef8bd4a074ae08
BenjaminPellieux <benjaminpellieux5@gmail.com>
2024-03-07 16:27:08 +0100

    WEB update

--- WEB/lib_topic.py
+++ WEB/lib_topic.py
@@ -1,9 +1,9 @@
 
 command_topic: dict ={
-                    "F": {"linear" : {"x": 1.0}}, 
-                    "B": {"linear": {"x": -1.0}}, 
-                    "R": {"angular": {"z": -1}}, 
-                    "L": {"angular": {"z": 1}}
+                    "F": {"linear" : {"x": 0.1}}, 
+                    "B": {"linear": {"x": -0.1}}, 
+                    "R": {"angular": {"z": -0.1}}, 
+                    "L": {"angular": {"z": 0.1}}
                     }
 
 commande_move: dict = {"linear": { 
@@ -20,6 +20,17 @@ commande_move: dict = {"linear": {
 
 topic_type_dict: dict ={  "/odometry/filtered": "nav_msgs/Odometry",
                            "/navsat/fix" :  "sensor_msgs/NavSatFix"}
+zone_topic: dict={"header": {
+                            "seq": None,
+                            "stamp":None,
+                            "frame_id": None
+                             },
+                  "point": {
+                            "x":0.0,
+                            "y":0.0,
+                            "z":0.0,
+                            }
+                  }
 
 
 # command_topic: dict = {
