e334bc49a73df82dd44ddb906a733f9a4f677566
ISTY <tomcavan78@gmail.com>
2024-03-21 14:05:44 +0100

    Compilation packages catkin

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -5,15 +5,12 @@
 # it emulates the modifications of the setup script without recurring computations
 
 # new environment variables
-export CMAKE_PREFIX_PATH='/home/koala/Desktop/PIF/catkin_ws/devel'
-export LD_LIBRARY_PATH='/home/koala/Desktop/PIF/catkin_ws/devel/lib'
-export PKG_CONFIG_PATH='/home/koala/Desktop/PIF/catkin_ws/devel/lib/pkgconfig'
-export PYTHONPATH='/home/koala/Desktop/PIF/catkin_ws/devel/lib/python3/dist-packages'
-export ROS_DISTRO='Debian'
-export ROS_ETC_DIR='//etc/ros'
-export ROS_PACKAGE_PATH='/home/ros/PIF/catkin_ws/src:/home/koala/Desktop/PIF/catkin_ws/src'
-export ROS_PYTHON_VERSION='3'
-export ROS_VERSION='1'
 
 # modified environment variables
-export PWD='/home/koala/Desktop/PIF/catkin_ws/build'
+export CMAKE_PREFIX_PATH="/home/ros/PIF/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/ros/PIF/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/ros/PIF/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/ros/PIF/catkin_ws/build'
+export PYTHONPATH="/home/ros/PIF/catkin_ws/devel/lib/python3/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ros/PIF/catkin_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/ros/PIF/catkin_ws/src:/home/koala/Desktop/PIF/catkin_ws/src:$ROS_PACKAGE_PATH"
