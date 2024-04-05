d75acc444b90a8904300ed0f83405ee0761a72c2
Benjamin PELLIEUX <benjaminpellieux5@gmail.com>
2024-04-05 10:40:14 +0200

    Add Video flux for project

--- catkin_ws/devel/share/pif_gps_convert/cmake/pif_gps_convertConfig.cmake
+++ catkin_ws/devel/share/pif_gps_convert/cmake/pif_gps_convertConfig.cmake
@@ -68,13 +68,13 @@ set(pif_gps_convert_CONFIG_INCLUDED TRUE)
 # set variables for source/devel/install prefixes
 if("TRUE" STREQUAL "TRUE")
   set(pif_gps_convert_SOURCE_PREFIX /home/ros/PIF/catkin_ws/src/pif_gps_convert)
-  set(pif_gps_convert_DEVEL_PREFIX /home/ros/PIF/catkin_ws/build/devel)
+  set(pif_gps_convert_DEVEL_PREFIX /home/ros/PIF/catkin_ws/devel)
   set(pif_gps_convert_INSTALL_PREFIX "")
   set(pif_gps_convert_PREFIX ${pif_gps_convert_DEVEL_PREFIX})
 else()
   set(pif_gps_convert_SOURCE_PREFIX "")
   set(pif_gps_convert_DEVEL_PREFIX "")
-  set(pif_gps_convert_INSTALL_PREFIX /usr/local)
+  set(pif_gps_convert_INSTALL_PREFIX /home/ros/PIF/catkin_ws/install)
   set(pif_gps_convert_PREFIX ${pif_gps_convert_INSTALL_PREFIX})
 endif()
 
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/ros/PIF/catkin_ws/build/devel/lib;/home/ros/catkin_ws/devel/lib;/opt/ros/noetic/lib)
+    foreach(path /home/ros/PIF/catkin_ws/devel/lib;/opt/ros/noetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
