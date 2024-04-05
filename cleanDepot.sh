5975361456e4749973ce65c2e4e1a437979634a6
BenjaminPellieux <benjaminpellieux5@gmail.com>
2024-04-05 17:44:00 +0200

    Delet useless and opti cleanning

--- cleanDepot.sh
+++ cleanDepot.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-
-echo $(find . \( -name "CMakeFiles" -o -name "CMakeCache.txt" -o -name "Makefile" -o -name "catkin_generated" \))  
-rm -rf $(find . \( -name "CMakeFiles" -o -name "CMakeCache.txt" -o -name "Makefile" -o -name "catkin_generated" \))
+declare todelet=$(find . \( -name "CMakeFiles" -o -name "CMakeCache.txt" -o -name "Makefile" -o -name "catkin_generated" -o -name "build" \))
+echo $todelet 
+rm -rf $todelet
 
 
